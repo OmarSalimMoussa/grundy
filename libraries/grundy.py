381345f02d681181d49ec26e0084a2e66f26c0f6
Omar Moussa <moussa.omar.salim@gmail.com>
2024-10-30 00:50:10 -0700

    Add a fib and prime check func.

--- libraries/grundy.py
+++ libraries/grundy.py
@@ -5,7 +5,15 @@ def my_function(n):
         result += i * i
     return result
 
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
 def run():
     """Main function to demonstrate profiling."""
     n = 10000000
     result = my_function(n)
+    result += fib(15)
+    print(result)
