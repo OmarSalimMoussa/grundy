2f425a7898231082e9f3930becd752f0838beb36
Omar Moussa <moussa.omar.salim@gmail.com>
2024-10-30 00:18:23 -0700

    Add some funcs

---
+++ libraries/grundy.py
@@ -0,0 +1,11 @@
+def my_function(n):
+    """Performs a computationally intensive task."""
+    result = 0
+    for i in range(n):
+        result += i * i
+    return result
+
+def run():
+    """Main function to demonstrate profiling."""
+    n = 10000000
+    result = my_function(n)
---
+++ main.py
@@ -0,0 +1,8 @@
+from libraries import cookie, grundy
+
+def main():
+    cookie.run()
+    grundy.run()
+
+if __name__ == '__main__':
+    main()
