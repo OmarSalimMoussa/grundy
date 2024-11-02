834ad9e88fb9692eb86bbbb2a2eab9f93665a877
Omar Moussa <moussa.omar.salim@gmail.com>
2024-11-02 01:54:08 -0700

    optimization by profiler

--- libraries/grundy.py
+++ libraries/grundy.py
@@ -1,22 +1,25 @@
 def my_function(n):
     """Performs a computationally intensive task."""
-    result = 0
-    for i in range(n):
-        result += i * i
-    return result
-
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, n - 1):
-        if n % i == 0:
-            return False
-    return True
-
+    # Using sum() with a generator expression is more efficient than a loop
+    return sum(i * i for i in range(n))
 
 def run():
     """Main function to demonstrate latency profiling."""
     n = 10000000
-    result = my_function(n)
-    result += int(is_prime(10001011))
+    # Avoiding unnecessary variable assignment
+    return my_function(n) + int(is_prime(10001011))
+
+def is_prime(n):
+    # Optimized primality test
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
